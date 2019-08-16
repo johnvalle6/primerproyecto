e67915f45749fc9f3eb0be67222e787a0ce3442d
johnvalle6 <johnvalle829@hotmail.com>
2019-08-16 13:15:19 -0500

    Add files via upload

---
+++ #main.js
@@ -0,0 +1,13 @@
+const name=john;
+edad=26;
+
+for (i=0; i<=28; i++) {
+
+
+    if (let i = 0; i <= edad; i++) {
+      if(i == edad) {
+          document.write("Mi nombre es" name, "y mi edad es" , ${i}</h1>) 
+    }
+
+  }
+}
